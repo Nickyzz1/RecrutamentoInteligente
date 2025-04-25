53bc067286ae39073858bbf499c531fd40ad8e73
nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2025-04-25 15:02:58 -0300

    api initial configuration

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01bf40d1e2dde772e1b48a601e20959bd6f118e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b21c24b20e466a19cbb975f3f9f9d5bf901b18")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
