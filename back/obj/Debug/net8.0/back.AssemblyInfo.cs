8cb8676c0b5310d74b3b6f1817312eca7ad4b417
adriansito124 <adriangobara@gmail.com>
2025-04-30 17:09:06 -0300

    login iniciado

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b21c24b20e466a19cbb975f3f9f9d5bf901b18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b15784b289c54a348fd1eb8875452a670eaeaaf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- front/src/app/components/header/page.tsx
+++ front/src/app/components/header/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Button } from "@mui/material";
 import React from 'react';
+import { ROUTES } from "@/constants/routes"
 
 export const HeaderNotLogged = () => {
 
@@ -11,7 +12,7 @@ export const HeaderNotLogged = () => {
             <h1 className="text-[#F5991D] font-bold text-xl">Recruit</h1>
 
             <div className="flex w-full justify-end gap-2">
-                <Button variant="outlined">Entrar</Button>
+                <Button variant="outlined" href={ROUTES.login}>Entrar</Button>
                 <Button variant="contained" sx={{ color: 'white', backgroundColor: '#036D3C' }}>Cadastrar</Button>
             </div>
         </div>
