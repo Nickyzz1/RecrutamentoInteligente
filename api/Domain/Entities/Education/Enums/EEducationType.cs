add339a864e94f8217ada37788699225b5e1f48b
Eduardo Henrique Ribeiro da Silva <eduardohenriqueribeirobrasil@gmail.com>
2025-05-21 16:51:39 -0300

    Changed DTO structure

--- api/Domain/Entities/Education/Enums/EEducationType.cs
+++ api/Domain/Entities/Education/Enums/EEducationType.cs
@@ -2,5 +2,8 @@ namespace Api.Domain.Models;
 
 public enum EEducationType
 {
-    
+    BasicEducation = 0,
+    TechnicalCourse = 1,
+    Graduation = 2,
+    PostGraduation = 3
 }
--- api/Domain/Entities/VacancyAttribute/Enums/EAttributeType.cs
+++ api/Domain/Entities/VacancyAttribute/Enums/EAttributeType.cs
@@ -2,5 +2,7 @@ namespace Api.Domain.Models;
 
 public enum EAttributeType
 {
-    
+    Assignment = 0,
+    Benefit = 1,
+    Requirement = 2
 }
