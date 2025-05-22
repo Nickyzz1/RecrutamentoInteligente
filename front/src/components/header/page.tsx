c6b809f3e857fec2453064ae688d5de95dd3856d
nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2025-05-22 16:41:39 -0300

    'a'

--- front/src/components/header/page.tsx
+++ front/src/components/header/page.tsx
@@ -7,15 +7,17 @@ export const HeaderNotLogged = () => {
 
     return(
         <>
-        <div className="flex w-full h-15 shadow-md items-center px-6 bg-[#ffffff]">
-            <h1 className="text-[#036D3C] font-bold text-xl">Darede</h1>
-            <h1 className="text-[#F5991D] font-bold text-xl">Recruit</h1>
-
-            <div className="flex w-full justify-end gap-2">
-                <Button variant="outlined" href={ROUTES.login}>Entrar</Button>
-                <Button variant="contained" href={ROUTES.register} sx={{ color: 'white', backgroundColor: '#036D3C' }}>Cadastrar</Button>
+            <div className="flex w-full h-15 fixed shadow-md items-center md:px-6 px-2 bg-[#ffffff]">
+                <h1 className="text-[#036D3C] font-bold text-xl hidden sm:flex">Darede</h1>
+                <h1 className="text-[#F5991D] font-bold text-xl hidden sm:flex">Recruit</h1>
+                <h1 className="text-[#036D3C] font-bold text-xl flex sm:hidden">D</h1>
+                <h1 className="text-[#F5991D] font-bold text-xl flex sm:hidden">R</h1>
+                <div className="flex w-full justify-end gap-2">
+                    <Button variant="outlined" href={ROUTES.login}>Entrar</Button>
+                    <Button variant="contained" href={ROUTES.register} sx={{ color: 'white', backgroundColor: '#036D3C' }}>Cadastrar</Button>
+                </div>
             </div>
-        </div>
+            <div className="h-15"></div>
         </>
     )
 }
